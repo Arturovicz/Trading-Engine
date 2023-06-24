02d3f8664f1b5610816e3d19531c538336cb11c1
Valerij <thiccnessy@gmail.com>
2023-06-24 15:05:59 +0200

    Initated InstrumentCS and OrdersCS

---
+++ InstrumentCS/Security.cs
@@ -0,0 +1,7 @@
+﻿namespace TradingEngineServer.Instrument
+{
+    public class Security
+    {
+
+    }
+}
---
+++ OrdersCS/IOrderCore.cs
@@ -0,0 +1,9 @@
+﻿namespace OrdersCS
+{
+    public interface IOrderCore
+    {
+        public long OrderId { get; }
+        public long Username { get; }
+        public long SecurityId { get; }
+    }
+}
